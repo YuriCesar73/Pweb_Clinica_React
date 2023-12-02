9d45eedc75a46f7a244df60d89b48a854c06af5a
Yuri Cesar <ycesar4273@gmail.com>
2023-12-02 20:45:13 -0300

    Encontrando paciente e médico ao realizar o login

--- clinica/src/components/login/index.jsx
+++ clinica/src/components/login/index.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import API from "/src/API/api.jsx"
 
 function Login(){
 
@@ -8,7 +9,8 @@ function Login(){
         tipo: ""
     });
 
-    const handleInputChange = (e) => {
+    const handleInputChange = async (e)  => {
+        console.log("Cheguei aqui handleInput");
         const { id, value } = e.target;
         setLoginInfo((prevValues) => ({
           ...prevValues,
@@ -16,19 +18,63 @@ function Login(){
         }));
       };
 
+
+      const onSubmit = (e) => {
+        console.log("Cheguei aqui antes do prevent");
+        e.preventDefault();
+
+        console.log("Cheguei aqui");
+        let url;
+        if(loginInfo.tipo == "Medico"){
+            url = "medico-ms/medicos/listar/" + loginInfo.senha;
+        }
+        else {
+            url = "paciente-ms/pacientes/listar/" + loginInfo.senha;
+        }
+        
+        async function apiGetUser(){
+            API.get(url).then((response) => {
+                if(response.status == 200){
+                    console.log(Object.keys(response));
+                    console.log(response);
+                }
+                }).catch((error) => {
+                    console.log("Entrei no error");
+                    console.log(error)
+                    //console.log(error.data);
+                })
+    }
+     apiGetUser();
+            
+    };
+
+
     return(
         <div>
             <h1>Login</h1>
-            <input type="text" placeholder="Email" />
+            <form onSubmit={(e) => {onSubmit(e)}}>
+            <input 
+                type="text"
+                id="email"
+                placeholder="Insira seu email" 
+                value={loginInfo.email} 
+                onChange={handleInputChange}/>
             <br />
-            <input type="senha" placeholder={loginInfo.tipo == "" ? "Insira sua senha" : loginInfo.tipo == "Paciente" ? "Insira seu cpf" : "Insira seu crm"} />
+            <input 
+                type="password"
+                id="senha" 
+                value={loginInfo.senha} 
+                onChange={handleInputChange} 
+                placeholder={loginInfo.tipo === "" ? "Insira sua senha" : loginInfo.tipo === "Paciente" ? "Insira seu cpf" : "Insira seu crm"}/>
             <br />
             <select id="tipo" name="tipo" required placeholder="Você é um(a): " value={loginInfo.tipo} onChange={handleInputChange}>
                 <option value="" disabled>Selecione uma opção</option>
                 <option value="Medico">Medico</option>
                 <option value="Paciente">Paciente</option>
             </select>
-            <button>Enviar</button>
+            <button type="submit">Enviar</button>
+            </form>
+            
             <button>Cadastrar-se</button>
         </div>
         
